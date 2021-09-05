f00a13007e6ebd34e5c3af47eadb62ff51df755e
SanskritiPandey <khushiksp17@gmail.com>
2021-09-05 00:10:26 -0700

    feat: js updated

--- app.js
+++ app.js
@@ -2,4 +2,27 @@ var outputText = document.querySelector("#output")
 var inputTxt = document.querySelector("#txt")
 var button = document.querySelector("#btn")
 
-var url =""
+
+var url ="https://api.funtranslations.com/translate/oldenglish.json"
+function getTranslationURL(text){
+    return url + "?" + "text=" + text
+}
+function geterror(error){
+    console.log("error occured",error)
+}
+function clickeventhandler(){
+
+    var txtInput = inputTxt.value;
+    fetch(getTranslationURL(txtInput))
+
+        .then(response => response.json())
+        .then(json =>{
+            var transtext =json.contents.translated;
+            outputText.innerText = transtext;
+        })
+            
+           
+        .catch(geterror)
+    };
+    btn.addEventListener("click", clickeventhandler) //adding event listener
+
